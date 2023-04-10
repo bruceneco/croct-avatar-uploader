6d50bb08da52b39b37f2d79203c228dd1334d833
Bruce Neco <bruce.neco@gmail.com>
2023-04-10 16:11:36 -0300

    test(useDropFiles): reset function

--- src/hooks/useDropFiles/useDropFiles.test.ts
+++ src/hooks/useDropFiles/useDropFiles.test.ts
@@ -1,36 +1,53 @@
 import { assert, test } from "vitest";
 import { act, renderHook } from "@testing-library/react";
 import useDropFiles from "./useDropFiles";
+import { Simulate } from "react-dom/test-utils";
 
-test("useDropFiles returns expected initial state", () => {
-  const { result } = renderHook(() => useDropFiles());
-
-  assert.deepEqual<File[]>(result.current.files, []);
-  assert.equal<boolean>(result.current.dragging, false);
-});
-
-test("useDropFiles updates state when file is dropped", () => {
-  const { result, rerender } = renderHook(() => useDropFiles());
-
+describe("useDropFiles", () => {
   const file = new File([], "test.txt", { type: "text/plain" });
+
+  const dragEvent = {
+    preventDefault() {},
+  } as DragEvent;
   const dropEvent = {
     preventDefault() {},
     dataTransfer: { files: [file] },
   } as unknown as DragEvent;
-  act(() => result.current.dropZoneProps.onDrop(dropEvent));
-  assert.deepEqual<File[]>(result.current.files, [file]);
-  assert.equal<boolean>(result.current.dragging, false);
-});
 
-test("useDropFiles sets dragging state when drag event occurs", () => {
-  const { result, rerender } = renderHook(() => useDropFiles());
-  const dragEvent = {
-    preventDefault() {},
-  } as DragEvent;
+  test("should return expected initial state", () => {
+    const { result } = renderHook(() => useDropFiles());
 
-  act(() => result.current.dropZoneProps.onDragLeave(dragEvent));
-  assert.equal<boolean>(result.current.dragging, false);
+    assert.deepEqual<File[]>(result.current.files, []);
+    assert.equal<boolean>(result.current.dragging, false);
+  });
 
-  act(() => result.current.dropZoneProps.onDragOver(dragEvent));
-  assert.equal<boolean>(result.current.dragging, true);
+  test("should update state when file is dropped", () => {
+    const { result, rerender } = renderHook(() => useDropFiles());
+
+    act(() => result.current.dropZoneProps.onDrop(dropEvent));
+    assert.deepEqual<File[]>(result.current.files, [file]);
+    assert.equal<boolean>(result.current.dragging, false);
+  });
+
+  test("should set dragging state when drag event occurs", () => {
+    const { result } = renderHook(() => useDropFiles());
+
+    act(() => result.current.dropZoneProps.onDragLeave(dragEvent));
+    assert.equal<boolean>(result.current.dragging, false);
+
+    act(() => result.current.dropZoneProps.onDragOver(dragEvent));
+    assert.equal<boolean>(result.current.dragging, true);
+  });
+  it("should reset dragging and files on call reset", () => {
+    const {
+      result: {
+        current: { reset, dropZoneProps, dragging, files },
+      },
+    } = renderHook(() => useDropFiles());
+    act(() => dropZoneProps.onDrop(dropEvent));
+    act(() => dropZoneProps.onDragOver(dragEvent));
+    act(() => reset());
+    assert.deepEqual(files, []);
+    assert.isFalse(dragging);
+  });
 });
