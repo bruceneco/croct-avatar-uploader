f7a20093d6d9d213a8cc40ef50c2ff6689d5b222
Bruce Neco <bruce.neco@gmail.com>
2023-04-10 18:01:55 -0300

    test(Card): close icon

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,16 +1,31 @@
 import Card from "./Card";
-import { assert, describe, test } from "vitest";
 import { customRender } from "../../utils/testing/customRender";
+import { fireEvent } from "@testing-library/react";
 
 describe("Card", () => {
-  test("should render without dotted outline by default", async () => {
+  it("should render without dotted outline by default", async () => {
     const { container } = customRender(<Card>Hello, World!</Card>);
     const mainElement = container.querySelector("div");
     assert.equal(mainElement?.style.outline, "");
   });
-  test('should render with dotted outline when "dotted" prop is true', async () => {
+  it('should render with dotted outline when "dotted" prop is true', async () => {
     const { container } = customRender(<Card dotted>Hello, World!</Card>);
     const mainElement = container.querySelector("div");
     assert.notEqual(getComputedStyle(mainElement!).outline, "");
+  });
+  it("should display close button if function is passed", () => {
+    const onClose = vi.fn();
+    const { getByAltText } = customRender(
+      <Card onClose={onClose}>Hello, World!</Card>
+    );
+    assert.exists(getByAltText("Close icon"));
+  });
+  it("should call onClose on close button click", () => {
+    const onClose = vi.fn();
+    const { getByAltText } = customRender(
+      <Card onClose={onClose}>Hello, World!</Card>
+    );
+    fireEvent.click(getByAltText("Close icon"));
+    expect(onClose).toHaveBeenCalled();
   });
 });
