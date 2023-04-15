298051a8a4dc4c4f97a3056becd83326fdb1d04a
Bruce Neco <bruce.neco@gmail.com>
2023-04-15 14:23:05 -0300

    refactor(LogoPreview): use img tag instead of styled component

--- src/components/AvatarUploader/components/LogoPreview/LogoPreview.tsx
+++ src/components/AvatarUploader/components/LogoPreview/LogoPreview.tsx
@@ -1,19 +1,32 @@
 import * as S from "./LogoPreview.styles";
 import attention from "../../../../assets/attention.svg";
+import {Ref} from "react";
 
 interface LogoPreviewProps {
-  invalid?: boolean;
-  src?: string;
+    invalid?: boolean;
+    src?: string;
+    zoom?: number;
+    imageRef?: Ref<any>;
+    onLoad?: () => void;
 }
 
 export default function LogoPreview({
-  invalid = false,
-  src,
-}: LogoPreviewProps) {
-  return (
-    <S.Wrapper>
-      {invalid && <img src={attention} alt="Warning icon" />}
-      {!invalid && <S.Logo src={src} alt="Logo preview from user" />}
-    </S.Wrapper>
-  );
+                                        invalid = false,
+                                        src,
+                                        imageRef,
+                                        onLoad
+                                    }: LogoPreviewProps) {
+    return (
+        <S.Wrapper>
+            {invalid && <img src={attention} alt="Warning icon"/>}
+            {!invalid && (
+                <img
+                    onLoad={onLoad}
+                    src={src}
+                    ref={imageRef}
+                    alt="Logo preview from user"
+                />
+            )}
+        </S.Wrapper>
+    );
 }
